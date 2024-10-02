724af1b2bd889f6d568d1013ad5eeac3769b91ae
codehunt022 <arunnath055@gmail.com>
2024-10-02 22:40:56 +0530

    redis

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,4 +1,4 @@
 import Redis from "ioredis"
-
-const client = new Redis("rediss://default:AWRIAAIjcDEyMWI4MjY5ZjFmMWU0ZmU4YjA0NjJhNDEzYmJhZDI2M3AxMA@glad-beetle-25672.upstash.io:6379");
-await client.set('foo', 'bar');
+import dotenv from "dotenv";
+const client = new Redis(process.env.UPSTASH_REDIS_URL);
+//await client.set('foo', 'bar');
