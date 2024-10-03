4e6bf40d5f979d597b735962dc7f676010755896
codehunt022 <arunnath055@gmail.com>
2024-10-03 11:46:00 +0530

    authentication generatation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,14 @@
  import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateTokens = (userId)=>{
+  const accessToken = jwt.sign({userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn: "15m",});
+  const refreshToken = jwt.sign({userId},process.env.REFRESH_TOKEN_SECRET,{
+    expiresIn:"7d",
+  });
+  return {accessToken,refreshToken};
+}
+
  export const signup =async (req, res) => {
 try {
   const {email,password,name}= req.body;
@@ -7,6 +17,7 @@ const userExists = await User.findOne({email});
 if(userExists) 
   return res.status(400).json({message:"User already exists"});
 const user = await User.create({name,email,password});
+//authenticate
   res.status(201).json({user, message:"user created successfully"});
 }
   catch (error) {
