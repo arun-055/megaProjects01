be9aad7c790106311227943a2d84fadd49316be8
codehunt022 <arunnath055@gmail.com>
2024-10-06 12:52:18 +0530

    product.delete route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,6 @@
+import cloudinary from "../lib/cloudinary.js";
 import Product from "../models/product.model.js";
+import User from "../models/user.model.js";
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({});
@@ -31,24 +33,50 @@ export const getFeaturedProducts = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
-export const createProduct = async (req,res)=>{
-    try {
-        const{name,description,price, image, category} = req.body;
-        let cloudinaryResponse=null;
-        if(image){
-            cloudinaryResponse= await cloudinaryResponse.uploader.upload(image,{folder:"products"})
-        }
-        const product = await Product.create({
-            name,
-            description,
-            price,
-            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
-            category
-        })
-        res.status(201).json(product)
-       
-    } catch (error) {
-        console.log("Error in createProduct controller", error.message);
-        res.status(500).json({message:"server error"})
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+    let cloudinaryResponse = null;
+    if (image) {
+      cloudinaryResponse = await cloudinaryResponse.uploader.upload(image, {
+        folder: "products",
+      });
     }
-}
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse.secure_url
+        : "",
+      category,
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    console.log("Error in createProduct controller", error.message);
+    res.status(500).json({ message: "server error" });
+  }
+};
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if(!product) {
+        return res.status(404).json({message:"product not found"});
+    }
+   if(product.image){
+    const publicId = product.image.split("/").pop().split(".")[0];
+    try {
+        await cloudinary.uploader.destroy(`products/${publicId}`)
+        console.log("deleting image from cloudinary")
+    } catch (error) {
+        console.log("error deleting image from cloudinary", error)
+    }
+    
+   }
+
+await Product.findByIdAndDelete(req.params.id)
+  } catch (error) {
+    console.log("delete product error");
+    res.status(500).json({message: "delete error"});
+  }
+};
